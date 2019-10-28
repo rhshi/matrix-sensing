4170a7a1ca517325110cfe9c12b112fdc72d5058
Bobby Shi <bhshi@uchicago.edu>
2019-10-27 22:05:59 -0500

    added class

--- sims.py
+++ sims.py
@@ -2,12 +2,49 @@ import numpy as np
 from sklearn.preprocessing import normalize
 
 
-U = np.random.randn(100, 5)
-U_normalized = normalize(U, norm='l2', axis=0)
+class MatrixSensing(object):
+	def __init__(self, d, r, eta):
+		m = 5 * d * r
+		self.matrices = [np.random.randn(d, d) for _ in range(m)]
 
-print(U_normalized)
+		U = np.random.randn(d, r)
+		U_normalized = normalize(U, norm='l2', axis=0)
 
-A = np.matmul(U_normalized, U_normalized.T)
-print(A)
-print(np.linalg.norm(A, ord=2))
-print(np.linalg.matrix_rank(A))
+		self.X_star = np.matmul(U_normalized, U_normalized.T)
+
+		self.eta = eta
+		self.d = d
+
+	def M_t(self, U):
+		total = np.zeros((self.d, self.d))
+		for A in self.matrices:
+			X = np.matmul(U, U.T)
+			matrix_product = np.matmul(A.T, X - self.X_star)
+			total += np.linalg.norm(matrix_product) * A
+
+		return total / len(self.matrices)
+
+	def step(self, U)
+		return np.matmul(np.identity - self.eta * self.M_T(U), U)
+
+	def train_error(self, U):
+		numerator = 0
+		denominator = 0
+		for A in self.matrices:
+			b = np.linalg.norm(np.matmul(A.T, self.X_star))
+			X = np.matmul(U, U.T)
+			numerator += (np.linalg.norm(np.matmul(A.T, X)) - b) ** 2
+			denominator += b ** 2
+
+		return np.sqrt(numerator / denominator)
+
+	def test_error(self, U):
+		X = np.matmul(U, U.T)
+		return np.linalg.norm(X - self.X_star) / np.linalg.norm(self.X_star)
+
+	def go(self, alpha, iters):
+		U = alpha * np.identity(self.d)
+		for _ in range(iters):
+			U = self.step(U)
+
+		return U
