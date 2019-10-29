89b19521dfac47e8bae0103b41be35e416c4150f
Bobby Shi <bhshi@uchicago.edu>
2019-10-29 16:14:57 -0500

    added logs folder creation

--- utils.py
+++ utils.py
@@ -3,6 +3,10 @@ import os
 
 class Logger(object):
 	def __init__(self, log_file):
+
+		if not os.path.exists("logs"):
+			os.mkdir("logs")
+
 		self.log_file = "logs/{}.txt".format(log_file)
 		os.mknod(self.log_file)
 
